98d55067b50861981d723b6cccae4b28b7d932fe
Produkten <Prototyp av produkten>
2019-10-30 11:58:08 +0100

    add: new css/background image, remove: images files not used

--- src/tables/ToxicTable.js
+++ src/tables/ToxicTable.js
@@ -11,13 +11,13 @@ const ToxicTable = (props) => (
                 <th><span role="img" aria-label="skull-and-love">💀 / 😍</span></th>
                 <th>Label</th>
                 <th>Toxic points</th>
-                <th>Delete</th>
+                <th>Delete a sentence</th>
+                <th>Delete all sentences</th>
             </tr>
             </thead>
             <tbody>
                     {props.sentences.map((input) => (
                         <tr key={input.index > 0 ? input.index : null}>
-
                             <td>{input.index}</td>
                             <td>{input.sentence}</td>
                             <td>{input.prediction}</td>
@@ -26,8 +26,10 @@ const ToxicTable = (props) => (
                             <td>{input.point}</td>
 
                             <td>
-                                <button onClick={() => props.deleteSentence(input.index)}>Delete this sentence</button>
-                                <button onClick={() => props.deleteAll(input.index)}>Delete all sentences</button>
+                                <button onClick={() => props.deleteSentence(input.index)}>Delete</button>
+                            </td>
+                            <td>
+                                <button onClick={() => props.deleteAll(input.index)}>Delete all</button>
                             </td>
                         </tr>
                     ))}
